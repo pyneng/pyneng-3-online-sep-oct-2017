6114cca3f5e92b66d37a37a61bcc320dd1345037
pyneng <pyneng.course@gmail.com>
2017-11-12 10:13:24 +0000

    Small changes after lecture

--- examples/14_textfsm/parse_output.py
+++ examples/14_textfsm/parse_output.py
@@ -1,6 +1,7 @@
 import sys
 import textfsm
 from tabulate import tabulate
+from pprint import pprint
 
 template = sys.argv[1]
 output_file = sys.argv[2]
@@ -9,5 +10,5 @@ with open(template) as f, open(output_file) as output:
     re_table = textfsm.TextFSM(f)
     header = re_table.header
     result = re_table.ParseText(output.read())
-    print(result)
+    pprint([header]+result)
     print(tabulate(result, headers=header))
