2a26def14f4aa46e218102d8ebb03f91df4d14bc
pyneng <pyneng.course@gmail.com>
2017-11-12 10:55:08 +0000

    Изменено задание 14.6

--- exercises/14_textfsm/task_14_6.py
+++ exercises/14_textfsm/task_14_6.py
@@ -2,14 +2,14 @@
 '''
 Задание 14.6
 
-Это задание похоже на задание 14.5, но в этом задании подключения надо выполнять параллельно.
+Это задание похоже на задание 14.5, но в этом задании подключения надо выполнять параллельно с помощью потоков. Для параллельного подключения использовать модуль concurrent.futures.
 
-Для этого надо использовать функции connect_ssh и conn_processes (пример из раздела multiprocessing) и функцию parse_command_dynamic из упражнения 14.4.
+Для этого надо использовать функцию connect_ssh (в задании) и функцию parse_command_dynamic из упражнения 14.4.
 
 В этом упражнении нужно создать функцию send_and_parse_command_parallel:
-* она должна использовать внутри себя функции connect_ssh, conn_processes и parse_command_dynamic
+* она должна использовать внутри себя функции connect_ssh и parse_command_dynamic
 * какие аргументы должны быть у функции send_and_parse_command_parallel, нужно решить самостоятельно
- * но надо иметь в виду, какие аргументы ожидают три готовые функции, которые используются
+ * но надо иметь в виду, какие аргументы ожидают готовые функции, которые используются
 * функция send_and_parse_command_parallel должна возвращать словарь, в котором:
  * ключ - IP устройства
  * значение - список словарей (то есть, тот вывод, который был получен из функции parse_command_dynamic)
@@ -18,49 +18,24 @@
 
 Проверить работу функции send_and_parse_command_parallel на команде sh ip int br.
 
-Пример из раздела multiprocessing:
 '''
 
-import multiprocessing
-import sys
 import yaml
 from pprint import pprint
 
 from netmiko import ConnectHandler
 
 
-COMMAND = sys.argv[1]
+test_command = "sh ip int br"
 devices = yaml.load(open('devices.yaml'))
 
 
-def connect_ssh(device_dict, command, queue):
+def connect_ssh(device_dict, command):
     with ConnectHandler(**device_dict) as ssh:
         ssh.enable()
         result = ssh.send_command(command)
 
         print('Connection to device {}'.format(device_dict['ip']))
-        queue.put({device_dict['ip']: result})
+    return {device_dict['ip']: result}
 
-
-def conn_processes(function, devices, command):
-    processes = []
-    queue = multiprocessing.Queue()
-
-    for device in devices:
-        p = multiprocessing.Process(target=function,
-                                    args=(device, command, queue))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
-
-    results = []
-    for p in processes:
-        results.append(queue.get())
-
-    return results
-
-
-pprint(conn_processes(connect_ssh, devices['routers'], COMMAND))
 
